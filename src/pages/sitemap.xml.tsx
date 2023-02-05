25fc7544621cbc4485b8f42a3d5910de28767723
Pavel Astapov <p.astapov@itransition.com>
2023-02-05 19:04:08 +0300

    sitema.xml

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,34 +1,30 @@
 import axios from "axios"
+import { GetServerSideProps } from "next";
 import { useEffect, useState } from "react";
 
 function Sitemap(){
-  const [sitemap, setSitemap] = useState<any>()
-
-  const getSitemap = async function Test(){
-    const response = await axios.get("https://strapi-production-93bb.up.railway.app/sitemap/index.xml");
-
-    return response.data;
-  }
-
-  useEffect(() => {
-    const menuData = async () => {
-      const fetchedData = await getSitemap();
-
-      setSitemap(fetchedData)
-    }
-
-    menuData()
-  }, [])
-
   return(
     <>
-      {sitemap && (
-        <div
-          dangerouslySetInnerHTML={{__html: sitemap}}
-        />
-      )}
+
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const { data } = await axios.get("https://strapi-production-93bb.up.railway.app/sitemap/index.xml");
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    ${data.split('.xsl"?>').pop()}
+  `
+
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
+
+  return {
+    props: {}
+  }
 }
 
 export default Sitemap;
